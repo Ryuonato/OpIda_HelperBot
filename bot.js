dc168273d6c438401a8a0f13a19c8c5e4bb19739
Ryuonato <Ryuonato@gmail.com>
2019-01-27 00:45:04 -0800

    added ping/pong messaging

--- bot.js
+++ bot.js
@@ -1,15 +1,28 @@
-const Discord = require('discord.js')
-var auth = require('./auth.json')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var auth = require('./auth.json');
+
 
 client.on('ready', () => {
-	console.log("Connected as " + client.user.tag)
+	console.log("Connected as " + client.user.tag);
 
-	client.user.setActivity("Op Ida Daily Update")
+	client.user.setActivity('Op Ida Daily Update');
 
 	client.guilds.forEach((guild) => {
-		console.log(guild.name)
-	})
-})
+		console.log(guild.name);
+	});
+});
 
-client.login(auth.token)
+client.on('message', function (msg) {
+	//Prevents botception, also known as a bot spamming loop.
+	if(message.author.bot) return;
+
+	if (msg.content.indexOf('!ping') === 0) {
+		bot.sendMessage(msg.channel, 'Pong!');
+
+		console.log('pong-ed ' + msg.author.username);
+	}
+
+});
+
+client.login(auth.token);
