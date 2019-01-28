146a8ba4a619273d6b4e8eeebdba9189616d5a7a
Ryuonato <Ryuonato@gmail.com>
2019-01-28 04:54:26 -0800

    added UTC date string to console log of ping/pong

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 var auth = require('./auth.json');
-
+var date = new Date().toUTCString();
 
 client.on('ready', () => {
 	console.log("Connected as " + client.user.tag);
@@ -11,6 +11,7 @@ client.on('ready', () => {
 	client.guilds.forEach((guild) => {
 		console.log(guild.name);
 	});
+	console.log (date);
 });
 
 client.on('message', msg => {
@@ -18,13 +19,13 @@ client.on('message', msg => {
 	if (msg.content === '!ping') {
 		msg.reply('Pong!');
 
-		console.log('pong-ed ' + msg.author.username + ' ' + Date.now());
+		console.log('pong-ed ' + msg.author.username + ' ' +  date);
 	}
 
 	if (msg.content === '!pong') {
 		msg.reply('Ping!');
 
-		console.log('ping-ed ' + msg.author.username + ' ' + Date.now());
+		console.log('ping-ed ' + msg.author.username + ' ' + date);
 	}
 
 });
